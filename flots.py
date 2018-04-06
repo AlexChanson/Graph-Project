aceb9fdd8f9f87756af09a885a459147fd70cd6b
Alexandre Chanson <a.chanson.tours@gmail.com>
2018-04-06 10:42:04 +0200

    dummy random flots generation

--- flots.py
+++ flots.py
@@ -1,11 +1,12 @@
 from pprint import pprint
 
+
 # Prend un graphe et retourne le flot nul correspondant
 def getFlotNul(graph):
     flot = dict()
     for key in graph.keys():
         l = []
-        #pprint(graph[key])
+        # pprint(graph[key])
         for e in graph[key]:
             l.append((e[0], (e[1][0], e[1][1], e[1][2], 0)))
         flot[key] = l
@@ -21,20 +22,20 @@ def busacker_gowen(graph, entre, sortie):
     C = 0
     found = True
     while found:
-        p, d = dijkstra(Graph(graph), entre, lambda x: x[3]*x[2])
+        p, d = dijkstra(Graph(graph), entre, lambda x: x[3] * x[2])
         current = p[sortie]
         chain = [current, sortie]
-        #print(p)
+        # print(p)
         while current is not entre:
             current = p[current]
             chain.insert(0, current)
-        #print("Chain", chain)
+        # print("Chain", chain)
         deltas = list()
         for i in range(len(chain) - 1):
-            suivant = list(filter(lambda x : x[0] == chain[i+1], graph[chain[i]]))[0]
-            #print(suivant)
+            suivant = list(filter(lambda x: x[0] == chain[i + 1], graph[chain[i]]))[0]
+            # print(suivant)
             deltas.append(suivant[1][1] - suivant[1][3])
-        #print("Deltas", deltas)
+        # print("Deltas", deltas)
         delta = min(deltas)
         print("d", delta)
         if delta <= 0:
@@ -49,6 +50,11 @@ def busacker_gowen(graph, entre, sortie):
             pprint(graph)
 
             V += delta
-            C += delta*len(chain)
+            C += delta * len(chain)
     return graph
+
+
+def rd_flots():
+    from random import randint
+    return randint(0, 5), randint(5, 10), randint(0, 7)
 

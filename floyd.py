f92c27745ce55de945af74156c6b4a348827c456
Alexandre Chanson <a.chanson.tours@gmail.com>
2018-04-07 11:59:45 +0200

    wrote flyod algorithm (not finished)

--- floyd.py
+++ floyd.py
@@ -1,2 +1,25 @@
 import numpy as np
+from graph import *
 
+
+def cout(graph, i, j):
+    pass
+
+
+def floyd(graph):
+    n = graph.countNodes()
+    p = np.zeros(shape=(n, n))
+    d = np.empty(shape=(n, n))
+    d[:] = np.inf
+
+    for i in range(n):
+        for j in range(n):
+            d[i, j] = cout(graph, i, j)
+            p[i, j] = i
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if d[i, k] + d[k, j] < d[i, j]:
+                    d[i, j] = d[i, k] + d[k, j]
+                    p[i, j] = k

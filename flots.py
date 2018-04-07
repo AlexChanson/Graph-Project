d6d36845bc36249e4f73accca32f1c0d710e0c69
Ben Crulis <bencrulis@gmail.com>
2018-04-07 18:12:14 +0200

    fixing things again

--- flots.py
+++ flots.py
@@ -4,25 +4,13 @@ from graph import Graph
 
 # Prend un graphe et retourne le flot nul correspondant
 def getFlotNul(graph):
-    flot = dict()
-    for key in graph.keys():
-        l = []
-        # pprint(graph[key])
-        for e in graph[key]:
-            l.append((e[0], (e[1][0], e[1][1], e[1][2], 0)))
-        flot[key] = l
-
-    return flot
-
-
-
-    print(r)
+    return graph.fmap(lambda x: (x[0], x[1], x[2], 0) )
 
 def pathReconstruction(p, u, v):
-    if u is v:
+    if u == v:
         return []
     path = [u]
-    while u is not v:
+    while u != v:
         u = p[(u,v)]
         path.append(u)
     return path
@@ -33,28 +21,26 @@ def busacker_gowen(graph, entre, sortie):
     C = 0
     found = True
     while found:
-        p, d = floyd2(Graph(graph).fmap(lambda x: x[2]*x[3]))
+        p, d = floyd2(graph.fmap(lambda x: (x[1]-x[2])*x[3]))
 
         chain = pathReconstruction(p, entre, sortie)
 
         deltas = list()
         for i in range(len(chain) - 1):
-            suivant = list(filter(lambda x: x[0] == chain[i + 1], graph[chain[i]]))[0]
-            # print(suivant)
-            deltas.append(suivant[1][1] - suivant[1][3])
+            suivant = graph.getEdgeValue(chain[i], chain[i+1])
+            deltas.append(suivant[1] - suivant[3])
         print(chain)
         print("Deltas", deltas)
         delta = min(deltas)
-        #print("d", delta)
+        print("d", delta)
         if delta <= 0:
             found = False
         else:
             for i in range(len(chain) - 1):
-                suivant = list(filter(lambda x: x[0] == chain[i + 1], graph[chain[i]]))[0]
-                new = (suivant[0], (suivant[1][0], suivant[1][1], suivant[1][2], suivant[1][3] + delta))
+                suivant = graph.getEdgeValue(chain[i], chain[i+1])
+                new = (suivant[0], suivant[1], suivant[2], suivant[3] + delta)
                 #print("Suivant", suivant, "New", new)
-                graph[chain[i]].remove(suivant)
-                graph[chain[i]].append(new)
+                graph.setEdgeValue(chain[i], chain[i+1], new)
             #pprint(graph)
 
             print(d[(entre, sortie)])

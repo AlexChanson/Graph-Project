c958cdd3d4b46cf790154d012a5bb9f65c546361
Alexandre Chanson <a.chanson.tours@gmail.com>
2018-04-09 19:12:12 +0200

    typos and legacy code fix

--- graphGen.py
+++ graphGen.py
@@ -1,6 +1,5 @@
-from random import randint, sample
+from random import randint, sample, choice
 from math import floor
-from pprint import pprint
 
 
 def rd_flots():
@@ -14,14 +13,14 @@ def random_level(nb_levels=5, nodes_per=(5, 10), connectivity=0.6, f=rd_flots):
         level = levels[i]
         q_nb = max(1, floor(floor(connectivity * (len(level) * len(levels[i + 1]))) / len(level)))
         for node in level:
-            availaible = set(levels[i + 1])
-            for j in range(min(q_nb, len(availaible))):
-                choosen = sample(availaible, 1)[0]
-                availaible.remove(choosen)
+            available = set(levels[i + 1])
+            for j in range(min(q_nb, len(available))):
+                chosen = sample(available, 1)[0]
+                available.remove(chosen)
                 if node in graph.keys():
-                    graph[node].append((choosen, f()))
+                    graph[node].append((chosen, f()))
                 else:
-                    graph[node] = [(choosen, f())]
+                    graph[node] = [(chosen, f())]
 
     graph["E"] = []
     graph["S"] = []
@@ -30,24 +29,25 @@ def random_level(nb_levels=5, nodes_per=(5, 10), connectivity=0.6, f=rd_flots):
     for node in levels[-1]:
         graph[node] = [("S", f())]
 
-    #pprint(graph)
     return graph
 
 
-def random(self, n, q, f=lambda x: randint(1, 10)):
+def random(n, q, f=lambda x: randint(1, 10)):
+    graph = {}
     for i in range(n):
-        self.graph[i] = []
+        graph[i] = []
     genenerated = 0
 
     def lam(x):
         return x[0]
     q = min(2*n*n, q)
-    all_nodes = list(self.graph.keys())
+    all_nodes = list(graph.keys())
     while genenerated < q:
         origin = choice(all_nodes)
         destination = origin
         while origin == destination:
             destination = choice(all_nodes)
-        if destination not in map(lam, self.graph[origin]):
+        if destination not in map(lam, graph[origin]):
             genenerated += 1
-            self.graph[origin].append((destination, f))
+            graph[origin].append((destination, f))
+    return graph

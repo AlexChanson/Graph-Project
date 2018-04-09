7b26c05018d9aa3cb09b2ed3aa87472ce876f4ee
Ben Crulis <bencrulis@gmail.com>
2018-04-09 19:37:30 +0200

    fixed graphe d'écart

--- busacker_gowen.py
+++ busacker_gowen.py
@@ -7,6 +7,8 @@ def getFlotNul(graph):
     return graph.fmap(lambda x: (x[0], x[1], x[2], 0) )
 
 def pathReconstruction(p, u, v):
+    if not p.get((u,v), False):
+        return []
     if u == v or p[(u,v)] == v:
         return []
     path = [u]
@@ -16,16 +18,16 @@ def pathReconstruction(p, u, v):
     return path
 
 def ecartGraph(graph):
-    newGraph = graph.fmap(id)
+    newGraph = graph.empty()
 
     for (nodeFrom, nodeTo, v) in graph.getAllEdges():
-        potential = v[1]-v[3]
-        if v[3] > 0:
-            newGraph.setEdgeValue(nodeTo, nodeFrom, (v[0], v[1], -v[2], v[3]))
+        min_, max_, cost, actual = v
+
+        potential = max_- actual
+        if actual > 0:
+            newGraph.setEdgeValue(nodeTo, nodeFrom,  -cost * actual)
         if potential > 0:
-            newGraph.setEdgeValue(nodeFrom, nodeTo, (v[0], v[1], v[2], potential))
-        else:
-            newGraph.removeEdge(nodeFrom, nodeTo)
+            newGraph.setEdgeValue(nodeFrom, nodeTo, cost * potential)
     return newGraph
 
 def busacker_gowen(graph, entre, sortie):
@@ -37,14 +39,18 @@ def busacker_gowen(graph, entre, sortie):
         graphEcartCalculated = ecartGraph(graph)
         #graphEcartCalculated.display()
 
-        p, d = floyd2(graphEcartCalculated.fmap(lambda x: x[2]))
+        graphEcartCalculated.display()
+        p, d = floyd2(graphEcartCalculated)
 
         chain = pathReconstruction(p, entre, sortie)
 
         deltas = list()
         for i in range(len(chain) - 1):
             suivant = graph.getEdgeValue(chain[i], chain[i+1])
-
+            if suivant is None:
+                print(chain)
+                pprint(p)
+                pprint(d)
             deltas.append(suivant[1] - suivant[3])
         #print("Deltas", deltas)
         delta = min(deltas, default=0)
